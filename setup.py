866b41dc68132975fee853afd52962b2a28cbd2e
shashidhar <shashisunny9030@gmail.com>
2026-01-15 16:48:15 +0530

    Experimentation (models and hyperparameters)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
-    author='Your name (or your organization/company/team)',
+    description='Production-grade MLOps platform demonstrating experiment tracking, automated ML pipelines, CI/CD, Kubernetes deployment, and monitoring using a customer value prediction use case.',
+    author='Shashidhar',
     license='',
 )
